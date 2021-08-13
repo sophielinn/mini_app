910721e10b67f26a87024933340c22a6f75759a3
Akshat Adsule <30205531+AkshatAdsule@users.noreply.github.com>
2021-08-12 17:19:54 -0700

    Create starter project

---
+++ android/app/src/main/kotlin/com/example/mustang_mini_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mustang_mini_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,1 @@
+// This is the start point of your application. Add a main method and create your root app widget here
